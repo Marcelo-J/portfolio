ac02d2fad4d3e7cb2d8a3e2961ffdcdd3e7626ad
Marcelo-J <ndehtaminang@gmail.com>
2023-06-23 20:19:55 +0100

    full backend

---
+++ config/app.config.js
@@ -0,0 +1,10 @@
+require("dotenv").config;
+
+const MONGO_DB_CONFIG={
+    DB:'mongodb+srv://ndehtaminang:jyhbvTFxwwdhl3Vk@reactbackendportfolio.x5hlxhb.mongodb.net/ReactBackendProject?retryWrites=true&w=majority'
+}
+
+
+module.exports = {
+    MONGO_DB_CONFIG
+}
